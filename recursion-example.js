d35516d036f0808e0bdd9f7605fb9ad857f6d559
jordanoboyle <jmcmcode@gmail.com>
2024-05-08 19:19:29 -0600

    return of n + (n - 1), proof of concept

--- recursion-example.js
+++ recursion-example.js
@@ -11,6 +11,12 @@ I won't pretend this is easy to visualize, but I will write a second recursive f
 Let's also annotate.*/
 
 function add(n) {
-  console.log("this function works")
+  return n + (n - 1)
 }
-add()
+
+console.log(add(4))
+
+// first make the function (this will go away with pratice)
+// now lets get the result n + (n - 1)
+// add conditional that says if n is 1, we don't use the function and return 1
+// add recursive function call in else statement  COMMIT EACH OF THESE CHANGES.
