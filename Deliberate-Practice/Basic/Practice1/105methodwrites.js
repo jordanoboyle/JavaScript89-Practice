717a210120f828e2a686ad8ca255176427256838
jordanoboyle <jmcmcode@gmail.com>
2024-05-23 20:30:51 -0600

    JavaScript function practice.

--- Deliberate-Practice/Basic/Practice1/105methodwrites.js
+++ Deliberate-Practice/Basic/Practice1/105methodwrites.js
@@ -42,15 +42,68 @@ subtractFirstFromSecond(18, 12);
 spaceBar();
 
 // # 4 Write a method that takes in a number and returns the number times itself. Then run the method and print the result.
+function multiplyYourself(num)  {
+  let product = num * num;
+  return console.log(product);
+}
+multiplyYourself(9);
+spaceBar();
+
 
 // # 5 Write a method that takes in a string and returns the first letter of the string. Then run the method and print the result.
+function getMeTheFirst(string2) {
+  let first = string2.charAt(0);
+  return console.log(first);
+}
+getMeTheFirst("Vader");
+spaceBar();
+
 
 // # 6 Write a method that takes in three strings and returns a string that combines all three strings with spaces in between. Then run the method and print the result.
+function combineTheseString(string1, string2, string3) {
+  let combined = string1 + string2 + string3;
+  return console.log(combined);
+}
+combineTheseString("these", "should", "combine");
+spaceBar();
+
 
 // # 7 Write a method that takes in a number and returns the number as a string. Then run the method and print the result.
-
+function returnAsAString(number) {
+  return String(number);
+}
+let numberString = returnAsAString(29839);
+console.log(typeof numberString);
+spaceBar();
 // # 8 Write a method that takes in a string and returns the string repeated 5 times. Then run the method and print the result.
+function fiveTimesRepeat(string4) {
+  let i = 0;
+  while (i < 5) {
+    console.log(string4);
+    i ++;
+  }
+}
+
+fiveTimesRepeat("repeat this!");
 
 // # 9 Write a method that takes in 3 numbers and returns the average (the sum divided by 3.0). Then run the method and print the result.
-
+function returnAverage(numArray)  {
+  let i = 0;
+  let sum = 0;
+  while (i < numArray.length) {
+    sum += numArray[i];
+    i ++;
+  }
+  return console.log(sum / numArray.length);
+}
+let needAve = [3, 7, 11, 19, 23];
+returnAverage(needAve);
+spaceBar();
 // # 10 Write a method that takes in a number and returns the number times 10 plus 30. Then run the method and print the result.
+function timesTenPlusThiry(number) {
+  return console.log((number * 10) + 30);
+}
+
+timesTenPlusThiry(50);
+
+
