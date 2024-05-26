4c0055d817bb4525bab28997b356589d83304a28
jordanoboyle <jmcmcode@gmail.com>
2024-05-25 22:54:03 -0600

    4 problems in matching conditions arrays

--- Deliberate-Practice/Intermediate/02transferelements.js
+++ Deliberate-Practice/Intermediate/02transferelements.js
@@ -4,17 +4,76 @@
 function spaceBar() {
   let space = "";
   for (let i = 0; i < 20; i++) {
-    
+    space += "$";
   }
+  return console.log(space);
 }
 
 // # 1 Start with an array of numbers and create a new array with only the numbers less than 20. For example, [2, 32, 80, 18, 12, 3] becomes [2, 18, 12, 3].
+let theseNumbers = [2, 32, 80, 18, 12, 3];
+let lessThanTwenty = [];
+theseNumbers.forEach(function(num) {
+  if (num < 20) {
+    lessThanTwenty.push(num);
+  }
+});
+console.log(lessThanTwenty);
+spaceBar();
+//option 2 with .filter()
+let lessThanTwenty2 = theseNumbers.filter(function(num) {
+  return num < 20;
+});
+console.log(lessThanTwenty2);
+spaceBar();
 
 // # 2 Start with an array of strings and create a new array with only the strings that start with the letter "w". For example, ["winner", "winner", "chicken", "dinner"] becomes ["winner", "winner"].
+let wordsOne = ["winner", "winner", "chicken", "dinner"];
+let startsWithW = [];
+wordsOne.forEach(word => {
+  if (word[0] === "w") {
+    startsWithW.push(word);
+  }
+});
+console.log(startsWithW);
+spaceBar();
+//option 2 .filter()
+let startsWithW2 = wordsOne.filter(word => {return word[0] === "w"} );  ///Freaking do not do this...just for fun
 
-// # 3 Start with an array of hashes and create a new array with only the hashes with prices greater than 5 (from the :price key). For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes [{name: "chair", price: 100}].
+console.log(startsWithW2);
+spaceBar();
+// # 3 Start with an array of hashes and create a new array with only the hashes with prices greater than 5 (from the :price key). For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "leather wallet", price: 15}, {name: "book", price: 4}] becomes [{name: "chair", price: 100}, {name: "leather wallet", price: 15}].
+let itemPrice = [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "leather wallet", price: 15}, {name: "book", price: 4}];
+let itemOver5 = [];
+itemPrice.forEach(item => {
+  if (item.price > 5) {
+    itemOver5.push(item.name);
+  }
+});
+console.log(itemOver5);
+spaceBar();
+//option2 .filter()
+let itemOver5Two = itemPrice.filter(function(item) {
+  return item.price > 5;
+});
+console.log(itemOver5Two);
+spaceBar();
 
 // # 4 Start with an array of numbers and create a new array with only the even numbers. For example, [2, 4, 5, 1, 8, 9, 7] becomes [2, 4, 8].
+let numbersOne = [2, 4, 5, 1, 8, 9, 7];
+let evensOne = [];
+numbersOne.forEach(num => {
+  if (num % 2 === 0) {
+    evensOne.push(num);
+  }
+});
+console.log(evensOne);
+spaceBar();
+
+//option 2 .filter()
+let evensTwo = numbersOne.filter(function(num) {
+  return num % 2 === 0;
+});
+console.log(evensTwo);
 
 // # 5 Start with an array of strings and create a new array with only the strings shorter than 4 letters. For example, ["a", "man", "a", "plan", "a", "canal", "panama"] becomes ["a", "man", "a", "a"].
 
