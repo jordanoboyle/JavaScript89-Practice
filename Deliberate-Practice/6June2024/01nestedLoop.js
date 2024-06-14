263f914dc2c2ab2528803d023fd22fc70ba6ebf8
jordanoboyle <jmcmcode@gmail.com>
2024-06-13 21:12:40 -0600

    greatest product problem complete

--- Deliberate-Practice/6June2024/01nestedLoop.js
+++ Deliberate-Practice/6June2024/01nestedLoop.js
@@ -33,10 +33,40 @@ spaceBar();
 // 3. Use a nested loop with one array of strings to create a new array that contains every combination of each string with every other string in the array.
 // For example, ["a", "b", "c", "d"] becomes ["ab", "ac", "ad", "ba", "bc", "bd", "ca", "cb", "cd", "da", "db", "dc"].
 
+let startLetters = ["a", "b", "c", "d"];
+let combineSingleLetArray = [];
+let i = 0;
+while (i < startLetters.length) {
+  let secondLetters = startLetters;
+  let j = 0;
+  while (j < secondLetters.length) {
+    if (startLetters[i] !== secondLetters[j]) {
+      combineSingleLetArray.push(startLetters[i] + secondLetters[j]);
+    }
+    j++;
+  }
+  i++;
+}
+console.log(combineSingleLetArray);
+spaceBar();
 
 
 // 4. Use a nested loop to find the largest product of any two different numbers within a given array.
 // For example, [5, -2, 1, -9, -7, 2, 6] becomes 63.
+let numbersOne = [5, -2, 1, -9, -7, 2, 6];
+let greatestProduct = 1;
+numbersOne.forEach(numOne => {
+  numbersOne.forEach(numTwo => {
+    if (numOne !== numTwo) {
+      let product = numOne * numTwo;
+      if (product > greatestProduct) {
+        greatestProduct = product;
+      }
+    }
+  });
+});
+console.log(greatestProduct);
+
 
 // 5. Use a nested loop to compute the sum of all the numbers in an array of number pairs.
 // For example, [[1, 3], [8, 9], [2, 16]] becomes 39.
