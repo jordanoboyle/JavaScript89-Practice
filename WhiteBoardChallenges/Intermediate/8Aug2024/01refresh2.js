2f7765e515ab5dae4bb7faa986eec91c9b98d9ed
jordanoboyle <jmcmcode@gmail.com>
2024-07-25 15:10:12 -0600

    01 resfresh.js, two problems practice with a do while

--- WhiteBoardChallenges/Intermediate/8Aug2024/01refresh2.js
+++ WhiteBoardChallenges/Intermediate/8Aug2024/01refresh2.js
@@ -1,7 +1,46 @@
+function spaceBar() {
+  let space = "";
+  for (let i = 0; i < 20; i++) {
+    space += "%";
+  }
+  return console.log(space);
+}
+
 // 5) Write a function that takes in an array of strings and joins them together to make a single string separated by commas.
+let joinThese = ["I", "am", "going", "to", "join", "these", "strings", "."];
+
+function joinStringArray(strings) {
+  let joinedString = "";
+  strings.forEach(element => {
+    if (strings.indexOf(element) === strings.length - 1) {
+      joinedString += element;
+    } else {
+      joinedString += element + ", ";
+    }
+  });
+  return console.log(joinedString);
+}
+
+joinStringArray(joinThese);
+
+spaceBar();
 
 // 6) Write a function that takes in an array of numbers and returns the product of all the numbers (each number multiplied by each other).
+let findProduct = [1, 2, 3, 4, 5]; //=> 96
 
+function getProductMath(numArray) {
+  let i = 0;
+  let product = 1;
+  do {
+    product *= numArray[i];
+    i ++;
+  }
+  while (i < findProduct.length);
+
+  return console.log(product);
+}
+getProductMath(findProduct);
+spaceBar();
 // 7) Write a function that takes in an array of numbers and returns the two smallest numbers.
 
 // 8) Write a function that takes in an array of numbers and returns a count of how many zeros are in the array.
