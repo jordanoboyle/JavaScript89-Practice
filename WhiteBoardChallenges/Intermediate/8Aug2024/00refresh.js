4dd0e300ca81a40a3d646c212c3e3b7a5e89ab2f
jordanoboyle <jmcmcode@gmail.com>
2024-07-25 12:58:57 -0600

    2 resfresh whiteboard challenge files

---
+++ WhiteBoardChallenges/Intermediate/8Aug2024/00refresh.js
@@ -0,0 +1,10 @@
+// Standard Technical:
+
+// 1) Write a function that takes in an array of numbers and returns its sum.
+
+// 2) Write a function that takes in an array of strings and returns the smallest string.
+
+// 3) Write a function that takes in an array of numbers and returns a new array with the numbers in reverse order.
+
+// 4) Write a function that takes in an array of words and returns the number of words that begin with the letter “a”.
+
---
+++ WhiteBoardChallenges/Intermediate/8Aug2024/01refresh2.js
@@ -0,0 +1,11 @@
+// 5) Write a function that takes in an array of strings and joins them together to make a single string separated by commas.
+
+// 6) Write a function that takes in an array of numbers and returns the product of all the numbers (each number multiplied by each other).
+
+// 7) Write a function that takes in an array of numbers and returns the two smallest numbers.
+
+// 8) Write a function that takes in an array of numbers and returns a count of how many zeros are in the array.
+
+// 9) Write a function that takes in an array of numbers and returns true if all the numbers are bigger than 10, otherwise returns false.
+
+// 10) Write a function that takes in an array of words and returns the number of times the letter “a” appeared in total.
