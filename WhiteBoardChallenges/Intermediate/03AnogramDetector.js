d3db3c99707f30ab07082817bf62737d18316aa7
jordanoboyle <jmcmcode@gmail.com>
2024-07-30 19:12:59 -0600

    solution using helper function to sort initial word, which impacts time complexit heavily. detectAnagram (whiteboard directory).

--- WhiteBoardChallenges/Intermediate/03AnogramDetector.js
+++ WhiteBoardChallenges/Intermediate/03AnogramDetector.js
@@ -17,11 +17,11 @@
 // push elements that create identical objects into their own array and return this array 
  
 
+ //BELOW WE ARE USING  Array.prototype.reduce; UNDER THE HOOD STILL A LOOP, BUT MORE ELEGANT THAN WRITING OUT ENTIRE LOOP TO ACCOMPLISH SAME THING (EXAMPLE OF THAT ON LINE 32)
 const word = "listen";
 const wordsArray = ["enlist", "google", "inlets", "banana"];
 
 function detectAnagogram(word, comparisonArray) {
-  //BELOW WE ARE USING  Array.prototype.reduce; UNDER THE HOOD STILL A LOOP, BUT MORE ELEGANT THAN WRITING OUT ENTIRE LOOP TO ACCOMPLISH SAME THING (EXAMPLE OF THAT ON LINE 32)
   let source = word.split("").reduce((acc, char) => {
     acc[char] = (acc[char] || 0) + 1;
     return acc;
@@ -58,11 +58,12 @@ function detectAnagogram(word, comparisonArray) {
       anagrams.push(comparisonArray[index]);
     }
     index += 1;
-  })
+  });
 
   return anagrams;
 }
 
+console.log("My clever solution that is slow, but does have use case theory in it.");
 console.log(detectAnagogram(word, wordsArray));
 
 
@@ -74,7 +75,63 @@ console.log(detectAnagogram(word, wordsArray));
 // }, {});
 // console.log(source)
 
-let obOne = {"h": 2, "j": 3};
-let obTwo = {"h": 2, "j": 3};
+// let obOne = {"h": 2, "j": 3};
+// let obTwo = {"h": 2, "j": 3};
 
-console.log(obOne === obTwo);
+// console.log(obOne === obTwo);
+
+
+//THIS IS A BETTER METHOD. ABOVE IS CLEVER, AND COULD BE USEFUL IN CERTAIN SPECIFIC USE CASES, BUT IT IS WILDY BAD IN TIME COMPLEXITY (TECHNICALLY IT IS 3N^4, BUT WE CAN MODIFY THIS USING THE SORT METHOD!)
+const wordTwo = "listen";
+const wordsArrayTwo = ["enlist", "google", "inlets", "banana"]; //=> enlist, inlets
+const word1 = "evil";
+const wordsArray1 = ["vile", "live", "veil", "villain", "levi"]; //==> vile, live, veil, levi
+const word2 = "conversation";
+const wordsArray2 = ["voices rant on", "consternation", "racon", "serotonin", "conserve a ton"]; // ==> no anagrams
+
+function detectAnagramTwo(wordToCompare, wordsArray) {
+  //we need a helper function here
+  const sortedWord = (word) => word.split("").sort().join();
+
+  let compString = sortedWord(wordToCompare);
+  let anagrams = [];
+
+  wordsArray.forEach(w => {
+    if (sortedWord(w) === compString) {
+      anagrams.push(w);
+    }
+  });
+
+  if (anagrams.length > 0) {
+    return anagrams;
+  } else {
+    return "There are no anagrams in the given dataset.";
+  }
+}
+
+console.log("USING HELPER FUNCTION sortWord to compare words");
+console.log(detectAnagramTwo(wordTwo, wordsArrayTwo));
+console.log(detectAnagramTwo(word1, wordsArray1));
+console.log(detectAnagramTwo(word2, wordsArray2));
+
+// You can also use the .filter() method here as well.  Let's build that real quick for some practice
+
+function detectAnagramThree (wordToCompare, wordsArray) {
+  // build our little helper function
+
+  const sortWord = (word) => word.split("").sort().join();
+  const sortedWord = sortWord(wordToCompare);
+
+  let anagrams = wordsArray.filter(w => sortWord(w) === sortedWord);
+
+  if (anagrams.length > 0) {
+    return anagrams;
+  } else {
+    return "This dataset does not have an anagram in it.";
+  }
+}
+
+console.log("Uses helper function, and .filter() method");
+console.log(detectAnagramThree(wordTwo, wordsArrayTwo));
+console.log(detectAnagramThree(word1, wordsArray1));
+console.log(detectAnagramThree(word2, wordsArray2));
