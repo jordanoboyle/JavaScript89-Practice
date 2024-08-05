de6bcb2f6148546db166894e66a7b6cc0d4d0ebc
jordanoboyle <jmcmcode@gmail.com>
2024-08-04 23:54:03 -0600

    class questions for aug4

---
+++ Deliberate-Practice/8August2024/00Classes.js
@@ -0,0 +1,9 @@
+// # 9 Write a Book class with attributes and reader/writer methods for title, author, and year. Then write a method that returns "Classic" if the book is older than 2000, otherwise it returns "Modern".
+
+
+
+// # 5 Write an Account class with attributes and reader/writer methods for name and balance. Then write a method that prints a warning if the balance is below $100.
+
+// # 7 Write a Car class with attributes and reader/writer methods for make, model, year, and color. Then write a method that returns the make and model as a single sentence in all lowercase letters.
+
+// 3. Write a Playlist class that stores a name and an array of songs with methods to add a song, remove a song, shuffle the songs into a random order, and display all the songs.
