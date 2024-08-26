c93c83afb2149cb11815e673c5cb8708ea855450
jordanoboyle <jmcmcode@gmail.com>
2024-08-25 21:35:09 -0600

    two pointer solution to palidrome, use while loop

--- Deliberate-Practice/8August2024/06TwoPointPalidrome.js
+++ Deliberate-Practice/8August2024/06TwoPointPalidrome.js
@@ -4,5 +4,27 @@
 
 let arr1 = [1, 2, 3, 2, 1];
 let arr2 = [1, 2, 3, 4, 5];
+let string = "tenet";
 
+function palindromeTwoPoint(arr) {
+  //use two pointer solution to find the solution
 
+  let leftPointer  = 0;
+  let rightPointer = arr.length - 1;
+  let isPalindrome = true;
+
+  while (leftPointer <= rightPointer) {
+    if (arr[leftPointer] !== arr[rightPointer]) {
+      isPalindrome = false;
+      break;
+    }
+    leftPointer ++;
+    rightPointer --;
+  }
+
+  return isPalindrome;
+}
+
+console.log(palindromeTwoPoint(arr1));
+console.log(palindromeTwoPoint(arr2));
+console.log(palindromeTwoPoint(string));
