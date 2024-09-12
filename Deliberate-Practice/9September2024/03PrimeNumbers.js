332cd1664e91d26c63006f18cb1dbd783d10da1a
jordanoboyle <jmcmcode@gmail.com>
2024-09-12 00:31:55 -0600

    prime number function, no recursion

---
+++ Deliberate-Practice/9September2024/03PrimeNumbers.js
@@ -0,0 +1,24 @@
+// 2. Write a function to generate/print/store the first “n” prime numbers.
+
+function findThePrimes(n) {
+  let primeArray = [];
+  let num = 2;
+
+  while (primeArray.length <= n) {
+    let isPrime = true;
+    for (let j = 2; j <= num - 1; j++) {
+      if (num % j === 0) {
+        isPrime = false;
+        break;
+      }  
+    }
+    if (isPrime) {
+      primeArray.push(num);
+    }
+    num++
+  }
+  return primeArray;
+}
+
+
+console.log(findThePrimes(11));
