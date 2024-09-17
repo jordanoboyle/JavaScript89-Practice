63f2e9f52b2d9793500b39f85456b9ce456bd6e1
jordanoboyle <jmcmcode@gmail.com>
2024-09-16 23:58:44 -0600

    lowest numnbers.

--- Deliberate-Practice/9September2024/05StringsAndSmallest.js
+++ Deliberate-Practice/9September2024/05StringsAndSmallest.js
@@ -41,3 +41,43 @@ console.log(joinStringsWithCommas(arr3));
 spaceBar();
 
 // 7) Write a function that takes in an array of numbers and returns the two smallest numbers.
+
+// Array 1: Length 10
+const arr4 = [-5, 3.7, -2, 4, 0.5, -8, 12, 6.1, 9, -0.2];
+
+// Array 2: Length 5
+const arr5 = [7.3, -3, 1.1, 0, -1];
+
+// Array 3: Length 2
+const arr6 = [-4.5, 2.9];
+
+
+function smallestTwoNumbers(numArray) {
+  if (numArray.length < 2) {
+    return "Array must have two elements.";
+  }
+
+  let small1 = numArray[0];
+  let small2 = numArray[0];
+
+  //get the smallest number
+  // use a HOAM forEach
+  // get the second smallest number\
+
+  numArray.forEach(val => {
+    if (val <= small1 && val < small1) {
+      small2 = small1;
+      small1 = val;
+      
+    } else if (val > small1 && val <= small2) {
+      small2 = val;
+    }
+  });
+  
+  let smallest = [small1, small2];
+  return smallest;
+}
+
+console.log(smallestTwoNumbers(arr4))
+console.log(smallestTwoNumbers(arr5))
+console.log(smallestTwoNumbers(arr6))
