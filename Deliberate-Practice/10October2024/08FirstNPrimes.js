72ea6e8a3294797f09b77944eb0de609f3b2dc4f
jordanoboyle <jmcmcode@gmail.com>
2024-10-23 23:54:14 -0600

    finding primes, a function

---
+++ Deliberate-Practice/10October2024/08FirstNPrimes.js
@@ -0,0 +1,26 @@
+// 2. Write a function to generate/print/store the first “n” prime numbers.
+
+function findFirstNPrimes(desiredPrimes) {
+  //you can do this with recursion, but in a lot of ways it is typically faster to not do so
+
+  let n = desiredPrimes;
+  let primes = [];
+  let start = 2;
+
+  while (n <= primes) {
+    let isPrime = true;
+    for(let j = 2; j < start - 1; j++) {
+      if (start % j === 0 ) {
+        isPrime = false;
+        break;
+      }
+    }
+    if (isPrime) {
+      primes.push(start);
+    }
+    num++;
+  }
+  return primes;
+}
+  
+  console.log(findFirstNPrimes(30))
