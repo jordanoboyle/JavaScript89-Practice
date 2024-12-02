80bec4ac049bb413149b1da1c4161abfc9862d4f
jordanoboyle <jmcmcode@gmail.com>
2024-12-01 19:40:53 -0700

    re-commit of the utils files. added 'type': 'module' to package.json in order to use importa export from ES6

--- SortingAlgosNotes/01SelectSort2.js
+++ SortingAlgosNotes/01SelectSort2.js
@@ -1,13 +1,7 @@
 //Practicing Selection sort
 
-// Array 1: Random positive integers and negative numbers
-const arr1 = [34, -2, 45, 1, -23, 16, 89, -5, 72, 10];
 
-// Array 2: Mixed positive and negative numbers with floating-point values
-const arr2 = [1.5, -3.2, 10, -15.5, 8.8, 5.1, -6.7, 12.9, -9.1, 3];
 
-// Array 3: Already sorted array (to test the best case for insertion sort)
-const arr3 = [-25, -12, -5, 0, 3, 10, 15, 22, 35, 48];
 
 function selectionSort(unsortedNumArr) {
   const n = unsortedNumArr.length;
---
+++ Utilities/utils.js
@@ -0,0 +1,39 @@
+
+export function selectionSort(unsortArr) {
+
+  let sortArr = unsortArr;
+  let n = sortArr.length;
+
+  for (let i = 0; i < n - 1; i++) {
+
+    let minimumIndex = i;
+
+    for (let j = i + 1; j < n; j++) {
+
+      if (sortArr[minimumIndex] > sortArr[j]) {
+        minimumIndex = j;
+      }
+    }
+
+    if (minimumIndex !== i) {
+      let temporary = sortArr[i];
+      sortArr[i] = sortArr[minimumIndex];
+      sortArr[minimumIndex] = temporary;
+    }
+  }
+  return sortArr;
+}
+
+export function eliminateDuplicates(arr) {
+  let checkAgainst = {};
+  let dupElim = [];
+
+  arr.forEach(value =>  {
+    if (checkAgainst[value] !== true) {
+      dupElim.push(value);
+      checkAgainst[value] = true;
+    }
+  });
+
+  return dupElim;
+}
